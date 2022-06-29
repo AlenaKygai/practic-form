050701039aeddd39ab086727db1d1478194eb284
Alena.Kygai <alena.kygai@gmail.com>
2022-06-29 16:02:20 +0300

    Add component Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import styles from './Header.module.css'
 
 const Header = () => {
   return (
-    <>
-      Header
-    </>
+    <header className={styles.header}>
+      <a href='/' className={styles.headerLogo}>
+        <img src='/static/images/logo.png' alt='logo'/>
+      </a>
+      <a href='/signUp' className={styles.headerButton}>
+        SignUp
+      </a>
+    </header>
   )
 }
 
