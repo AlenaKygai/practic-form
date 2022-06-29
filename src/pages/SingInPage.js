77d947f26c39074fc03847893df3b2889cb9976b
Alena.Kygai <alena.kygai@gmail.com>
2022-06-29 19:37:28 +0300

    Add component SingInPage

---
+++ src/pages/SingInPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Layout from '../components/Layout';
+import SignInForm from '../components/forms/SignInForm';
+
+const SingInPage = () => {
+  return (
+    <Layout>
+      <SignInForm />
+    </Layout>
+  )
+}
+
+export default SingInPage;
