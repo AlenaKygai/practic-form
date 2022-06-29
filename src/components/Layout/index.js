b6a0eef006796fbc12575c84edb4923bcd0b9424
Alena.Kygai <alena.kygai@gmail.com>
2022-06-29 16:02:43 +0300

    Add component Layout

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import Header from '../Header';
+import cx from 'classnames';
+import styles from './Layout.module.css'
+
 
 const Layout = ({children}) =>{
   return(
-    <>
-    <div>
+    <div className={styles.Layout}>
+      <div className='container'>
       <Header/>
+      <main>{children}</main>
+      </div>
     </div>
-    <main>{children}</main>
-    </>
   )
 }
 
