254f99244ea2b437daec2b5896362d07ef98af9a
Alena.Kygai <alena.kygai@gmail.com>
2022-07-11 20:11:42 +0300

    Refactor routers for Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,18 +2,27 @@ import React from 'react';
 import styles from './Header.module.css';
 import {
   Link,
+  useLocation,
 } from "react-router-dom";
 import {ROUTES} from '../../constants/index';
 
 const Header = () => {
+  const { pathname } = useLocation();
+
   return (
     <header className={styles.header}>
       <Link to={ROUTES.SIGN_IN} className={styles.headerLogo}>
         <img src='/static/images/logo.png' alt='logo'/>
       </Link>
-      <Link to={ROUTES.SIGN_UP} className={styles.headerButton}>
+
+      {pathname === ROUTES.SIGN_IN
+      ? <Link to={ROUTES.SIGN_UP} className={styles.headerButton}>
         SignUp
       </Link>
+      : <Link to={ROUTES.SIGN_IN} className={styles.headerButton}>
+        Login
+      </Link>      
+      }
     </header>
   )
 }
