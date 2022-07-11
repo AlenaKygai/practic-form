2fcce1579b87deaf41b0fdafa3758adf55b1db40
Alena.Kygai <alena.kygai@gmail.com>
2022-07-11 20:08:00 +0300

    Add validation style for inputs

--- src/components/forms/InputWrapper/index.js
+++ src/components/forms/InputWrapper/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import {Field,ErrorMessage} from 'formik';
 import styles from './InputWrapper.module.css';
-// import cx from 'classnames';
+import cx from 'classnames';
 
 const InputWrapper = (props) => {
-  const {name} = props;
+  const {name,...rest} = props;
   return(
-    <label>
-      <Field {...props} className={styles.inputWrapper}/>
+    <label className={styles.inputWrapper}>
+      <Field {...props}>{
+        ({field,meta})=>{
+          const classNames=cx(styles.input,{
+            [styles.invalid]: meta.touched && meta.error
+          })
+          return <input {...field} {...rest} className={classNames}/>
+        }
+      }
+      </Field>  
       <ErrorMessage name={name} component='span' className={styles.error}/>
     </label>
 
